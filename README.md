ff1a28c57bf8d5998a46e2dc559b09cfb251f4f9
Lilly <68789766+Paisseon@users.noreply.github.com>
2022-06-03 00:32:49 +0200

    Untested beta version
    Couldn't get testers, theoretically should work.

--- README.md
+++ README.md
@@ -4,26 +4,20 @@ For, um, educational purposes only or something. Definitely *don't* use this to 
 
 A modern in-app purchase cracker built for sideloaded applications, especially on unjailbroken devices. If you want to crack in-app purchases on jailbroken devices, including for sideloaded apps, try [Satella 2][1]
 
-## Usage
-1. Download Satella.dylib from here
-2. Obtain a **decrypted** .ipa of your target app
-3. Patch the .ipa to include Satella.dylib
+## Manual Method
+
+1. Obtain a *decrypted* .ipa of your target app
+2. `git clone https://github.com/Paisseon/SatellaJailed.git`
+3. `cd SatellaJailed && sh patch.sh <iOS version: 12, 13, 14, or 15>`. Optional arguments are -i for input file and -o for output file.
 4. Sideload the patched .ipa to your device
-5. Attempt to make an in-app purchase, then cancel on the popup
 
-Alternatively:
+## AppDB Method
+
 1. Link your device to [AppDB][2] 
-2. Enable the in-app purchase patch and install an app
-3. Attempt to make an in-app purchase, then cancel on the popup
+2. Enable the in-app purchase patch
+3. Install an app that has in-app purchases
 
-You can also access Satella Jailed’s preferences by long-pressing with two fingers inside an app. By default, receipts and observer are disabled.
-
-## Credits
-
-**ZonD80** The idea
-**Meep Meep, Nya, Shouri10, Slowie, and Sudo** Private beta testing
-**AppDB** On-device patching method
-**Artem Kasper** Help with preferences
+Now you can make fake purchases without a jailbreak! Just attempt a purchase and cancel on the popup. You can also access Satella Jailed’s preferences by long-pressing with two fingers inside an app if you are on iOS 13 or higher.
 
 [1]:	https://github.com/Paisseon/Satella2
 [2]:	https://appdb.to/?ref=cb9904cc802fa5380a7aa4c35fe0d0c1
---
+++ Swift (New)/Sources/SatellaC/include/Tweak.h
@@ -0,0 +1,71 @@
+#import <objc/runtime.h>
+#import <StoreKit/StoreKit.h>
+
+@interface RCEntitlementInfo : NSObject
+@property (readonly) NSString *identifier;
+@property (readonly) bool isActive;
+@property (readonly) bool willRenew;
+@property (readonly) NSDate *latestPurchaseDate;
+@property (readonly) NSDate *originalPurchaseDate;
+@property (readonly) NSDate *expirationDate;
+@property (readonly) NSString *productIdentifier;
+@property (readonly) bool isSandbox;
+- (void) setIdentifier: (NSString *) arg0;
+- (void) setIsActive: (bool) arg0;
+- (void) setWillRenew: (bool) arg0;
+- (void) setExpirationDate: (NSDate *) arg0;
+- (void) setOriginalPurchaseDate: (NSDate *) arg0;
+- (void) setLatestPurchaseDate: (NSDate *) arg0;
+- (void) setIsSandbox: (bool) arg0;
+@end
+
+@interface RCEntitlementInfos : NSObject
+@property (readonly) NSDictionary *active;
+@property (readonly) NSDictionary *all;
+- (void) setAll: (NSDictionary *) arg0;
+@end
+
+@interface RCPurchaserInfo : NSObject
+- (void) setEntitlements: (RCEntitlementInfos *) arg0;
+@end
+
+@interface NSObject (SatellaRevCat)
+- (void) setIdentifier: (NSString *) arg0;
+- (void) setIsActive: (bool) arg0;
+- (void) setWillRenew: (bool) arg0;
+- (void) setExpirationDate: (NSDate *) arg0;
+- (void) setOriginalPurchaseDate: (NSDate *) arg0;
+- (void) setLatestPurchaseDate: (NSDate *) arg0;
+- (void) setIsSandbox: (bool) arg0;
+@end
+
+@interface SKPaymentTransaction (Satella)
+- (void) _setTransactionState: (SKPaymentTransactionState) arg0;
+- (void) _setError: (NSError *) arg0;
+@end
+
+@interface SKProduct (Satella)
+- (void) _setPrice: (NSDecimalNumber *) arg0;
+- (void) _setPriceLocale: (NSLocale *) arg0;
+- (void) _setProductIdentifier: (NSString *) arg0;
+- (void) _setLocalizedDescription: (NSString *) arg0;
+- (void) _setLocalizedTitle: (NSString *) arg0;
+@end
+
+@interface SKProductsResponse (Satella)
+- (void) _setProducts: (NSArray *) arg0;
+@end
+
+NSArray<NSString *> *productIdentifiers(SKProductsRequest *arg0) {
+	id internalRequest        = [arg0 valueForKey: @"_productsRequestInternal"];
+	NSSet *productIdentifiers = [internalRequest valueForKey: @"_productIdentifiers"];
+	if (productIdentifiers) {
+		return [productIdentifiers allObjects];
+	} else {
+		return @[@"emt.paisseon.satella.cypwn"];
+	}
+}
+
+RCEntitlementInfo *blank_entitlement() {
+    return [[objc_getClass("RCEntitlementInfo") alloc] init];
+}
